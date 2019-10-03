0989d11c6d8bfd218cd5a1325eec34d996462dae
Oteranga <40015080+Oteranga@users.noreply.github.com>
2019-10-03 11:31:13 -0500

    Update Sort.h

--- Sort.h
+++ Sort.h
@@ -7,20 +7,3 @@
 
 #endif //PROYECTO_2_SORT_H
 
-#include <iostream>
-#include <vector>
-#include "elementos.h"
-using namespace std;
-
-template <typename T>
-
-class Sort{
-protected:
-    vector<T> v;
-public:
-    Sort(vector<T>);
-    //void swap(T& small, T large);
-    virtual void create()=0;
-    //bool *comp(T a, T b);
-
-};
